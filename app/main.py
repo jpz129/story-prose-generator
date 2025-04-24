7921a7bfe9e43d5dada2e7f9b05675ea797cd9f4
Juan Pablo Zamanillo <jpzamanillo@Juans-MacBook-Air.local>
2025-04-23 20:34:07 -0700

    update cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 # app/main.py
 from fastapi import FastAPI
 from app.api import story_generator
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # OR: ["http://localhost:4321"] for local dev only
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include API routes.
 app.include_router(story_generator.router, prefix="/api")
